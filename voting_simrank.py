3af31041968c5dc769349532b1faf791dcf48de0
Jesse Halpern <jesseoh@umich.edu>
2016-11-11 19:33:58 -0500

    Add files via upload

--- voting_simrank.py
+++ voting_simrank.py
@@ -20,21 +20,15 @@ if __name__ == "__main__":
 			reader = csv.DictReader('Voting_Data/'+folder+'/legislators.csv')
 			dataframes.append(list(reader))
 
-	nodes = copy.deepcopy(dataframes[0])
-	for node in nodes:
-		node.pop('yes_votes', None)
-		node.pop('no_votes', None)
-
-	g = graph.SimrankGraph
-	indexes = [i for i in range(len(nodes))]
-	for pair in itertools.combinations(indexes, 2):
+	nodes = [i for i in range(len(dataframes[0]))]
+	g = graph.SimrankGraph(nodes)
+	for pair in itertools.combinations(nodes, 2):
 		weight = 0
 		for i in range(2):
 			if 'yes_votes' in dataframes[i][pair[0]] and 'yes_votes' in dataframes[i][pair[1]]:
 				weight += len(set.intersection(dataframes[i][pair[0]]['yes_votes'], dataframes[i][pair[1]]['yes_votes']))
 			if 'no_votes' in dataframes[i][pair[0]] and 'no_votes' in dataframes[i][pair[1]]:
 				weight += len(set.intersection(dataframes[i][pair[0]]['no_votes'], dataframes[i][pair[1]]['no_votes']))
-		print(pair)
 		g.SetEdgeById(pair[0], pair[1], weight, False)
 
 	g.Run()
