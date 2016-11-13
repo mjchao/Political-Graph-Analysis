ce1ba35092d5ff6ea77708df8355fec600ae1b08
Mickey Chao <mjchao2@gmail.com>
2016-11-13 01:38:24 -0500

    Upload a more efficient simrank.

--- graph.py
+++ graph.py
@@ -1,3 +1,4 @@
+import collections
 import numpy as np
 
 class Graph(object):
@@ -15,7 +16,7 @@ class Graph(object):
         self._node_to_id = {nodes[i]: i for i in range(len(nodes))}
         self._id_to_node = {i: nodes[i] for i in range(len(nodes))}
 
-    def GetId(self, node):
+    def _GetId(self, node):
         """Gets the internal id in range [0, NUM_NODES) for a given node.
 
         Args:
@@ -27,7 +28,7 @@ class Graph(object):
         """
         return self._node_to_id[node]
 
-    def GetNode(self, node_id):
+    def _GetNode(self, node_id):
         """Gets the node given an internal id in range [0, NUM_NODES).
 
         Args:
@@ -54,7 +55,7 @@ class DenseGraph(Graph):
         super(DenseGraph, self).__init__(nodes)
         self._adj = np.zeros((len(nodes), len(nodes)), dtype=np.float32)
 
-    def SetEdgeById(self, from_id, to_id, weight=1.0, directed=True):
+    def _SetEdgeById(self, from_id, to_id, weight=1.0, directed=True):
         """Sets an edge between two nodes.
 
         Args:
@@ -78,31 +79,18 @@ class DenseGraph(Graph):
         """
         from_id = self._node_to_id[from_node]
         to_id = self._node_to_id[to_node]
-        self.SetEdgeById(from_id, to_id, weight, directed)
+        self._SetEdgeById(from_id, to_id, weight, directed)
 
-    def GetEdgeWeight(self, from_node=None, to_node=None, from_id=None,
-                        to_id=None):
-        """Gets the edge weight between two nodes. Either the nodes or the ids
-        should be specified but not both.
+    def GetEdgeWeight(self, from_node, to_node):
+        """Gets the edge weight between two nodes. 
 
         Args:
             from_node: (object) The source node.
             to_node: (object) The destination node.
-            from_id: (int) The internal node id for the source node.
-            to_id: (int) The internal node id for the destination node.
         """
-        if ((from_node is not None and to_node is not None) and
-                                        (from_id is None and to_id is None)):
-            from_id = self._node_to_id[from_node]
-            to_id = self._node_to_id[to_node]
-            return self._adj[from_id][to_id]
-
-        if ((from_node is None and to_node is None) and
-                                (from_id is not None and to_id is not None)):
-            return self._adj[from_id][to_id]
-
-        raise ValueError("Must specify (from_node, to_node)"
-                            "or (from_id, to_id), but not both")
+        from_id = self._node_to_id[from_node]
+        to_id = self._node_to_id[to_node]
+        return self._adj[from_id][to_id]
 
 
 class SimrankGraph(DenseGraph):
@@ -121,7 +109,19 @@ class SimrankGraph(DenseGraph):
         for i in range(len(self._nodes)):
             self._adj[i][i] = 1.0
 
-    def GetNeighbors(self, node=None, node_id=None):
+        self._dist = np.empty((len(nodes), len(nodes))) * np.nan
+        for i in range(len(nodes)):
+            self._dist[i][i] = 0
+
+        self._nodes_within_radius_invalidated = False
+        self._nodes_within_radius = [[] for _ in range(len(self._nodes))]
+        self._last_radius = 0
+
+    def _SetEdgeById(self, from_id, to_id, weight=1.0, directed=True):
+        super(SimrankGraph, self)._SetEdgeById(from_id, to_id, weight, directed)
+        self._nodes_within_radius_invalidated = True
+
+    def _GetNeighbors(self, node=None, node_id=None):
         """Gets the neighbors of the given node. Either the node or the node_id
         but not both should be specified. A neighbor of a given node has a
         directed edge coming into it from the given node. If a node links to
@@ -146,12 +146,67 @@ class SimrankGraph(DenseGraph):
             node_id = self._node_to_id[node]
         
         neighbors = []
-        for i in range(len(self._nodes)):
-            if self.GetEdgeWeight(from_id=node_id, to_id=i) != 0.0:
+        for i in self._nodes_within_radius[node_id]:
+            if self._adj[node_id][i] != 0.0:
                 neighbors.append(self._id_to_node[i])
         return neighbors
 
-    def Run(self, iterations=5, C=0.8):
+    def _ComputeNodesWithinRadius(self, r=None):
+        """Computes the nodes within a given radius for every node in the graph.
+        Applies a Breadth-First Search. The worst-case complexity is
+        O(|V|^3) but for small r, this complexity should be lower.
+
+        Args:
+            r: (int) The radius. If None, the radius used is infinity. If
+                negative, there will be no nodes in the neighbors list.
+        """
+        if not self._nodes_within_radius_invalidated and r == self._last_radius:
+            return
+
+        self._nodes_within_radius_invalidated = False
+        self._last_radius = r
+
+        if r is None:
+            self._nodes_within_radius = [range(len(self._nodes))
+                for _ in range(len(self._nodes))]
+            return
+
+        if r < 0:
+            self._nodes_within_radius = [[] for _ in range(len(self._nodes))]
+            return
+
+        self._nodes_within_radius = []
+        # Perform a breadth first search to find all nodes within radius r
+        # of each node in the graph.
+        for i in range(len(self._nodes)):
+            nodes_within_radius = [] 
+            added_to_queue = [False for _ in range(len(self._nodes))] 
+            dist = np.empty(len(self._nodes)) * np.nan
+            search_queue = collections.deque()
+            added_to_queue[i] = True
+            search_queue.append(i)
+            dist[i] = 0
+            while search_queue:
+                curr_node = search_queue.popleft()
+                for j in range(len(self._nodes)):
+                    if self._adj[curr_node][j] != 0.0:
+                        potential_dist = (dist[curr_node] +
+                                            self._adj[curr_node][j])
+                        if potential_dist > r:
+                            continue
+                        if np.isnan(dist[j]) or (not np.isnan(dist[j]) and
+                                                    potential_dist < dist[j]): 
+                            dist[j] = potential_dist
+                        if not added_to_queue[j]:
+                            search_queue.append(j)
+                            added_to_queue[j] = True
+            for i in range(len(self._nodes)):
+                if added_to_queue[i]:
+                    nodes_within_radius.append(i)
+            self._nodes_within_radius.append(nodes_within_radius)
+
+
+    def Run(self, iterations=5, C=0.8, r=None):
         """Runs the SimRank algorithm to determine the node similarity between
         all pairs of nodes. Note: similarity is not reset after each call to
         this function, so you can continue running for more iterations later on.
@@ -159,12 +214,14 @@ class SimrankGraph(DenseGraph):
         Args:
             iterations: (int) Number of iterations for which to run.
         """
+        self._ComputeNodesWithinRadius(r)
+
         for iteration in range(iterations):
             next_similarity = np.zeros((len(self._nodes), len(self._nodes)))
             for i in range(len(self._nodes)):
-                for j in range(len(self._nodes)):
-                    node1_neighbors = self.GetNeighbors(node_id=i)
-                    node2_neighbors = self.GetNeighbors(node_id=j)
+                for j in self._nodes_within_radius[i]:
+                    node1_neighbors = self._GetNeighbors(node_id=i)
+                    node2_neighbors = self._GetNeighbors(node_id=j)
                     for node1_neighbor in node1_neighbors:
                         for node2_neighbor in node2_neighbors:
                             node1_id = self._node_to_id[node1_neighbor]
