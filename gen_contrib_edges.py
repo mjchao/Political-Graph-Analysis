177a384bf41619408735bb8834f9543ff6f33462
cvwang <charlesvwang@gmail.com>
2016-12-11 17:18:42 -0500

    Cleaned up files to take arguments. Created root makefile for submission.

--- gen_contrib_edges.py
+++ gen_contrib_edges.py
@@ -1,88 +1,97 @@
 from sets import Set
 import graph
+from optparse import OptionParser
+import sys
 
 """ Get contribution graph edges and node attributes
 """
 
-# nameToID = {}
-
-# # Tie politician name and ID
-# with open('contributions.csv', 'r') as f:
-#   for line in f:
-#     line = line[:-1]
-#     line = line.split(',')
-#     nameToID[line[0]] = line[1]
-
-IDToParty = {}
-
-# Tie politician IDs to party affiliation
-with open('legislators.csv', 'r') as f:
-  for line in f:
-    line = line[:-1]
-    # line = line.lower()
-    line = line.split(',')
-    politicianID = line[20]
-    politicianParty = line[7].lower()
-    IDToParty[politicianID] = politicianParty
-
-# Choose the congress session
-# years = Set([1998])
-# years = Set([2014])
-# years = Set([1998, 2000, 2002, 2004, 2006, 2008, 2010, 2012, 2016])
-years = Set([1998, 2000, 2002, 2004, 2006, 2008, 2010, 2012, 2014, 2016])
-
-yearToSession = {
-  1998 : 105,
-  2000 : 106,
-  2002 : 107,
-  2004 : 108,
-  2006 : 109,
-  2008 : 110,
-  2010 : 111,
-  2012 : 112,
-  2014 : 113,
-  2016 : 114
-}
-
-for year in years:
-  nodes = Set()
-  politicians = Set()
-  edges = []
-  with open('contributions.csv','r') as f:
+def generateContributionEdges(weight = False):
+  IDToParty = {}
+  # Tie politician IDs to party affiliation
+  with open('legislators.csv', 'r') as f:
     for line in f:
+      line = line[:-1]
+      # line = line.lower()
       line = line.split(',')
-      if int(line[2]) == year:
-        line[1] = line[1].upper()
-        nodeName = line[0] + ',' + line[1]
-        if line[1] in IDToParty:
-          nodeName = nodeName + ',' + IDToParty[line[1]]
-        politicians.add(nodeName)
-        nodes.add(nodeName)
-        nodes.add(line[4])
-        
-        # Use this line for unweighted graph
-        # edges.append([nodeName,line[4]])
+      politicianID = line[20]
+      politicianParty = line[7].lower()
+      IDToParty[politicianID] = politicianParty
 
-        # Use this line for weighted graph
-        weight = float(line[-4])
-        # NOTE: needed to added this as node2vec was not playing well with non-positive weights.
-        if weight > 0.0:
-          edges.append([nodeName,line[4],weight])
+  # Choose the congress session
+  years = Set([1998, 2000, 2002, 2004, 2006, 2008, 2010, 2012, 2014, 2016])
 
-  print 'Number of nodes: ' + str(len(nodes))
-  print 'Number of edges: ' + str(len(edges))
+  yearToSession = {
+    1998 : 105,
+    2000 : 106,
+    2002 : 107,
+    2004 : 108,
+    2006 : 109,
+    2008 : 110,
+    2010 : 111,
+    2012 : 112,
+    2014 : 113,
+    2016 : 114
+  }
 
-  contribution_graph = graph.SparseGraph(list(nodes))
-  for edge in edges:
-    contribution_graph.SetEdge(edge[0], edge[1], weight=edge[2], directed=True)
+  for year in years:
+    nodes = Set()
+    politicians = Set()
+    edges = []
+    with open('contributions.csv','r') as f:
+      for line in f:
+        line = line.split(',')
+        if int(line[2]) == year:
+          line[1] = line[1].upper()
+          nodeName = line[0] + ',' + line[1]
+          if line[1] in IDToParty:
+            nodeName = nodeName + ',' + IDToParty[line[1]]
+          politicians.add(nodeName)
+          nodes.add(nodeName)
+          nodes.add(line[4])
+          
+          # Use this line for weighted graph
+          if weight:
+            weight = float(line[-4])
+            # NOTE: needed to added this as node2vec was not playing well with non-positive weights.
+            if weight > 0.0:
+              edges.append([nodeName,line[4],weight])
+          else:
+            edges.append([nodeName,line[4]])
 
-  # # Save graph adjacency list to file
-  # contribution_graph.SaveAdjacencyList('node2vec/contribution_edges_%d.txt' % yearToSession[year], weight=False)
-  # # Save node ID mapping
-  # contribution_graph.SaveNodeMapping('node2vec/contribution_id_map_%d.txt' % yearToSession[year])
+    print 'Number of nodes: ' + str(len(nodes))
+    print 'Number of edges: ' + str(len(edges))
 
-  # Print 
-  # Save graph adjacency list to file
-  contribution_graph.SaveAdjacencyList('node2vec/contribution_edges_weight_%d.txt' % yearToSession[year], weight=True)
-  # Save node ID mapping
-  contribution_graph.SaveNodeMapping('node2vec/contribution_id_map_%d.txt' % yearToSession[year])
+    contribution_graph = graph.SparseGraph(list(nodes))
+    if weight:
+      for edge in edges:
+        contribution_graph.SetEdge(edge[0], edge[1], weight=edge[2], directed=True)
+    else:
+      for edge in edges:
+        contribution_graph.SetEdge(edge[0], edge[1], directed=True)
+
+    if weight:
+      # Save graph adjacency list to file w/ weight
+      contribution_graph.SaveAdjacencyList('node2vec/contribution_edges_weight_%d.txt' % yearToSession[year], weight=True)
+    else:
+      # Save graph adjacency list to file
+      contribution_graph.SaveAdjacencyList('node2vec/contribution_edges_%d.txt' % yearToSession[year], weight=False)
+    # Save node ID mapping
+    contribution_graph.SaveNodeMapping('node2vec/contribution_id_map_%d.txt' % yearToSession[year])
+
+def main():
+  parser = OptionParser()
+  parser.add_option('-w', '--weight', action='store_true', dest='weight',
+                      help='print weighted contribution edges.')
+  options, args = parser.parse_args()
+
+  if options.weight:
+    print 'Is weight'
+    generateContributionEdges(weight = True)
+  else:
+    print 'Is not weight'
+    generateContributionEdges(weight = False)
+
+if __name__ == '__main__':
+  main()
+  sys.exit(0)
--- node2vec/tsne_contribution.py
+++ node2vec/tsne_contribution.py
@@ -1,70 +1,94 @@
+""" Usage
+
+Example:
+
+python tsne_contribution.py -f unwighted_graph/contribution_node2vec_out_105.txt -g unwighted_graph/contribution_id_map_105.txt
+"""
+
 import numpy as np
 import matplotlib.pylab as plt
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
+from optparse import OptionParser
+import sys
 
-# Adjust this value for session value
-session = 114
+def runTSNE(vec_file, ground_truth_file):
+  X = np.empty([1])
 
-X = np.empty([1])
+  print 'Reading in feature matrix file...'
+  # Read in node2vec into np.array
+  with open(vec_file, 'r') as f:
+    numNodes, numFeatures = map(int, (f.readline()[:-1]).split(' '))
+    print 'Number of nodes: ' + str(numNodes)
+    print 'Number of features: ' + str(numFeatures)
+    X = np.empty([numNodes, numFeatures])
+    for line in f:
+      nodeValues = map(float, line[:-1].split(' '))
+      X[int(nodeValues[0])] = nodeValues[1:numFeatures+1]
 
-print 'Reading in feature matrix file...'
-# Read in node2vec into np.array
-with open('weighted_graph/contribution_node2vec_out_%d.txt' % session, 'r') as f:
-  numNodes, numFeatures = map(int, (f.readline()[:-1]).split(' '))
-  print 'Number of nodes: ' + str(numNodes)
-  print 'Number of features: ' + str(numFeatures)
-  X = np.empty([numNodes, numFeatures])
-  for line in f:
-    nodeValues = map(float, line[:-1].split(' '))
-    X[int(nodeValues[0])] = nodeValues[1:numFeatures+1]
+  # TODO may want to run PCA before TSNE to reduce the feature dimension down to the suggested 50 features
+  print 'Reducing node2vec down to 50 feature PCA vector'
+  model = PCA(n_components=50, random_state=None)
+  pca = model.fit_transform(X)
+  # Y = model.fit_transform(X)
+  print 'Modelling t-SNE...'
+  model = TSNE(n_components=2, random_state=None)
+  Y = model.fit_transform(pca)
 
-# TODO may want to run PCA before TSNE to reduce the feature dimension down to the suggested 50 features
-print 'Reducing node2vec down to 50 feature PCA vector'
-model = PCA(n_components=50, random_state=None)
-pca = model.fit_transform(X)
-# Y = model.fit_transform(X)
-print 'Modelling t-SNE...'
-model = TSNE(n_components=2, random_state=None)
-Y = model.fit_transform(pca)
-
-x_coords = Y[:, 0]
-y_coords = Y[:, 1]
-is_democrat = []
-is_republican = []
-with open("weighted_graph/contribution_id_map_%d.txt" % session, 'r') as f:
-  for line in f.readlines():
-    data = line.rstrip("\n").split(",")
-    if len(data) > 3:
-      party = data[3]
-      if party == "democrat":
-        is_democrat.append(True)
-        is_republican.append(False)
-      elif party == "republican":
-        is_republican.append(True)
-        is_democrat.append(False)
+  x_coords = Y[:, 0]
+  y_coords = Y[:, 1]
+  is_democrat = []
+  is_republican = []
+  with open(ground_truth_file, 'r') as f:
+    for line in f.readlines():
+      data = line.rstrip('\n').split(',')
+      if len(data) > 3:
+        party = data[3]
+        if party == 'democrat':
+          is_democrat.append(True)
+          is_republican.append(False)
+        elif party == 'republican':
+          is_republican.append(True)
+          is_democrat.append(False)
+        else:
+          is_republican.append(False)
+          is_democrat.append(False)
       else:
         is_republican.append(False)
         is_democrat.append(False)
-    else:
-      is_republican.append(False)
-      is_democrat.append(False)
-    
-is_republican = np.array(is_republican)
-is_democrat = np.array(is_democrat)
+      
+  is_republican = np.array(is_republican)
+  is_democrat = np.array(is_democrat)
 
-republican_x = x_coords[is_republican]
-republican_y = y_coords[is_republican]
-democrat_x = x_coords[is_democrat]
-democrat_y = y_coords[is_democrat]
+  republican_x = x_coords[is_republican]
+  republican_y = y_coords[is_republican]
+  democrat_x = x_coords[is_democrat]
+  democrat_y = y_coords[is_democrat]
 
-is_company = np.logical_and(np.logical_not(is_republican), np.logical_not(is_democrat))
-company_x = x_coords[is_company]
-company_y = y_coords[is_company]
+  is_company = np.logical_and(np.logical_not(is_republican), np.logical_not(is_democrat))
+  company_x = x_coords[is_company]
+  company_y = y_coords[is_company]
 
-plt.scatter(company_x, company_y, color="g")
-plt.scatter(republican_x, republican_y, color="r", marker='^')
-plt.scatter(democrat_x, democrat_y, color="b", marker='.')
-plt.title("TSNE on Democrats, Republicans, and Companies %d" % session)
-plt.savefig('pca_tsne_weighted_contrib_%d.png' % session)
-plt.show()
+  plt.scatter(company_x, company_y, color='g')
+  plt.scatter(republican_x, republican_y, color='r', marker='^')
+  plt.scatter(democrat_x, democrat_y, color='b', marker='.')
+  plt.title('TSNE on Democrats, Republicans, and Companies')
+  plt.savefig('pca_tsne_weighted_contrib_%d.png')
+  plt.show()
+
+def main():
+  parser = OptionParser()
+  parser.add_option('-f', '--file', action='store', dest='vec_file',
+                        help='node vector file.')
+  parser.add_option('-g', '--ground_truth', action='store',
+                      dest='gt_file',
+                      help='file containing the ground truth.')
+
+  options, args = parser.parse_args()
+  print 'Processing Congress'
+  runTSNE(options.vec_file, options.gt_file)
+  print
+
+if __name__ == '__main__':
+  main()
+  sys.exit(0)
